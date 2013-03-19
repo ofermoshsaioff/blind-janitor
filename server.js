3851a4bc1a07c468690b66c952c60859ee4e2740
Ofer Moshaioff <ofer.moshaioff@gmail.com>
2013-03-19 16:59:31 +0200

    beautified code a little

--- server.js
+++ server.js
@@ -10,43 +10,41 @@ app.get('/', function (req, res) {
 });
 
 app.get('/reviews/:name', function (req, res) {
-	var path = 'reviews/' + req.params.name+ '/index.md';	
-	markdown(path, function (err, result) {
-	  if (err) {		
-		res.send(err);
-	  }
-	  res.render('review', {'body':result});
-	});
+  var path = 'reviews/' + req.params.name+ '/index.md';	
+  markdown(path, function (err, result) {
+    if (err) {		
+	  res.send(err);
+	}
+	res.render('review', {'body':result});
+  });
 });
 
 /* Gets the list of writers from the reviews.json file TODO -- need to handle duplicate names (maybe reduce?) */
 app.get('/writers', function (req, res) {
-	function iterator(item, callback) {
-		callback(null, item.reviewer);
-	}
-	
-	async.map(reviews, iterator, function(err, results) {
-		res.send(results);
-	});
+  function iterator(item, callback) {
+    callback(null, item.reviewer);
+  }	
+  async.map(reviews, iterator, function(err, results) {
+    res.send(results);
+  });
 });
 
 /* Gets the list of reviews for this writer's name */
 app.get('/writers/:name', function (req, res) {
-	function iterator(item, callback) {	
-		callback(item.reviewer == req.params.name);
-	}
-	
-	async.filter(reviews, iterator, function(results) {
-		res.send(results);
-	});
+  function iterator(item, callback) {	
+    callback(item.reviewer == req.params.name);
+  }	
+  async.filter(reviews, iterator, function(results) {
+    res.send(results);
+  });
 });
 		
 		
 
 /* The 404 Route (ALWAYS Keep this as the last route) */
 app.get('/*', function (req, res) {
-    console.log('got 404 request to ' + req.url);
-    res.render('404', {
-        layout: false
-    });
+  console.log('got 404 request to ' + req.url);
+  res.render('404', {
+    layout: false
+  });
 });
